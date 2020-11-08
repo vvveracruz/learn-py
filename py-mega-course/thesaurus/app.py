5f60ee8027535532213ee0ff9ec358429d278e23
veracruz <login@vgg.cz>
2020-11-08 10:17:03 +0000

    finished thesaurus app

--- py-mega-course/thesaurus/app.py
+++ py-mega-course/thesaurus/app.py
@@ -15,12 +15,6 @@ def getWord():
 
     return word.lower()
 
-def isValidWord( word, data):
-    if word in data:
-        return True
-    else:
-        return False
-
 def checkMatches( word, data):
     matches = get_close_matches( word, data )
     if matches == []:
@@ -28,21 +22,21 @@ def checkMatches( word, data):
     else:
         answer = input( "Did you mean " + matches[0] + "? (Y/N) " )
         if answer.upper() == "Y":
-            return matches[0]
+            return matches
         else:
             return None
 
 def getDefinition( word = '' ):
     data = getDictionary()
-
     word = getWord()
-    while isValidWord( word, data ) == False:
-        match = checkMatches( word, data )
-        if match == None:
-            print("It doesn't look like that is a word. Please try again.")
-            word = getWord()
-        else:
-            word = match
+
+    matches = checkMatches( word, data )
+    while matches == None:
+        print("It doesn't look like that is a word. Please try again.")
+        word = getWord()
+        matches = checkMatches( word, data )
+
+    word = matches[0]
 
     return "\n" + str(data[ word ]) + "\n\n"
 
