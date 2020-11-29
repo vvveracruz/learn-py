38eb870626c6a1d275c51d23403f77701f575bb7
vvveracruz <login@vgg.cz>
2020-11-29 08:42:50 +0000

    section 23 contd

--- py-mega-course/10-oop/backend.py
+++ py-mega-course/10-oop/backend.py
@@ -2,43 +2,43 @@ import sqlite3 as sq
 
 class Database:
     def __init__( self, db ):
-        conn = sq.connect( db )
-        cur = conn.cursor()
-
-        cur.execute( "CREATE TABLE IF NOT EXISTS book ( id INTEGER PRIMARY KEY, title text, author text, year integer, ISBN integer ) " )
-
-        conn.commit()
+        #   executed everytime you call an instance
+        self.conn = sq.connect( db )
+        self.cur = self.conn.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS book ( id INTEGER PRIMARY KEY, title text, author text, year integer, ISBN integer ) " )
+        self.conn.commit()
 
     def insert( self, title, author, year, ISBN ):
-        cur.execute( "INSERT INTO book VALUES ( NULL, ?, ?, ?, ? )", ( title, author, year, ISBN ) )
-
-        conn.commit()
-        conn.close()
+        self.cur.execute(
+            "INSERT INTO book VALUES ( NULL, ?, ?, ?, ? )",
+            ( title, author, year, ISBN ) )
+        self.conn.commit()
 
     def view( self ):
-        cur.execute( "SELECT * FROM book" )
-        rows = cur.fetchall()
-
-        conn.close()
-
+        self.cur.execute( "SELECT * FROM book" )
+        rows = self.cur.fetchall()
         return rows
 
     def search( self, title="", author="", year="", ISBN="" ):
-        cur.execute( "SELECT * FROM book WHERE title = ? OR author = ? OR year = ? OR ISBN = ?", (title, author, year, ISBN ) )
-        rows = cur.fetchall()
-
-        conn.close()
-
+        self.cur.execute(
+            "SELECT * FROM book WHERE title = ? OR author = ? OR year = ? OR ISBN = ?",
+            (title, author, year, ISBN ) )
+        rows = self.cur.fetchall()
         return rows
 
     def delete( self, id ):
-        cur.execute( "DELETE FROM book WHERE id = ?", (id, ) )
-
-        conn.commit()
-        conn.close()
+        self.cur.execute(
+            "DELETE FROM book WHERE id = ?",
+            (id, ) )
+        self.conn.commit()
 
     def update( self, id, title, author, year, ISBN ):
-        cur.execute( "UPDATE book SET title = ?, author = ?, year = ?, ISBN = ? WHERE id = ?", ( title, author, year, ISBN, id ) )
+        self.cur.execute(
+            "UPDATE book SET title = ?, author = ?, year = ?, ISBN = ? WHERE id = ?",
+            ( title, author, year, ISBN, id ) )
+        self.conn.commit()
 
-        conn.commit()
-        conn.close()
+    def __del__(self):
+        #   executed when the script is exited (the instance is deleted)
+        self.conn.close()
