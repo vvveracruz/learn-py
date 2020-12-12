044650f21f5772b1c11e654311a7b8ab263b675a
Ve Gomez <login@vgg.cz>
2020-12-12 14:19:57 +0000

    forgot to save before commiting 🤦‍♂

--- py-mega-course/16-webcam-motion-detector/script.py
+++ py-mega-course/16-webcam-motion-detector/script.py
@@ -10,6 +10,10 @@ while True:
     grey = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY )
     cv2.imshow( "Capturing", grey )
 
+    if first_frame == None:
+        first_frame = grey
+
+
     key = cv2.waitKey( 1 )
     print( grey )
 
