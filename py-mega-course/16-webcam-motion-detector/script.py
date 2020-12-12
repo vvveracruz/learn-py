6f6036eb85157978712bdd5f82d1da801a79e5d2
Ve Gomez <login@vgg.cz>
2020-12-12 14:19:26 +0000

    section 16

---
+++ py-mega-course/16-webcam-motion-detector/script.py
@@ -0,0 +1,21 @@
+import cv2, time
+
+first_frame = None
+
+
+video = cv2.VideoCapture( 0 )
+
+while True:
+    check, frame = video.read()
+    grey = cv2.cvtColor( frame, cv2.COLOR_BGR2GRAY )
+    cv2.imshow( "Capturing", grey )
+
+    key = cv2.waitKey( 1 )
+    print( grey )
+
+    if key == ord( 'q' ):
+        break
+
+print( a )
+video.release()
+cv2.destroyAllWindows()
